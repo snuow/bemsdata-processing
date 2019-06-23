091a30d73f66ab1fabebb814143880e23d4ceb88
snuow <24.nishi.yuuki@gmail.com>
2019-06-23 15:43:49 +0900

    added config.ini added .gitignore

--- Tools/tools_common.py
+++ Tools/tools_common.py
@@ -1,7 +1,19 @@
+import configparser as cp
+
+
 class Tools():
     def __init__(self):
         self.name = 'Tool1'
 
     def _folderpath(self):
         pass
-    
+
+
+def read_config() -> object:
+    config = cp.ConfigParser()
+    config.read(r'config.ini', encoding='Shift-JIS')
+    return config
+
+if __name__ == '__main__':
+    test = read_config()
+    print('completed')
